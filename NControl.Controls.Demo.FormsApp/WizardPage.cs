c4b8b624a89a22b016a29929f4f25cd84ce6150c
Christian Falch <christian.falch@gmail.com>
2016-04-21 16:07:46 +0200

    Added new control - Wizard Layout

---
+++ NControl.Controls.Demo.FormsApp/WizardPage.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace NControl.Controls.Demo.FormsApp
+{
+	public class WizardPage: ContentPage
+	{
+		public WizardPage ()
+		{
+			Title = "WizardLayout";
+		}
+
+		protected override void OnAppearing ()
+		{
+			base.OnAppearing ();
+
+			var wizard = new WizardLayout ();
+			wizard.Pages = new View[]{
+				new Button {
+					Command = new Command((obj)=>wizard.Page++),
+					Text = "Page 2",
+				},
+
+				new Button {
+					Command = new Command((obj)=>wizard.Page++),
+					Text = "Page 3",
+				},
+
+				new Button {
+					Command = new Command((obj)=>wizard.Page++),
+					Text = "Page 4",
+				},
+
+				new Button {
+					Text = "Done",
+				}
+			};
+
+			Content = wizard;
+		}
+	}
+}
+
